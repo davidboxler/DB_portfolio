23da3ea2ad3215232b017ef1fa8e1ccb25df716c
davidboxler <davidboxler47@gmail.com>
2024-06-16 18:14:16 -0300

    Se agregan cambios

--- js/form.js
+++ js/form.js
@@ -8,26 +8,23 @@ function sendEmail() {
   const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Message ${mess.value}`;
 
   Email.send({
-    SecureToken: "77c489c8-a51c-4cc5-814c-59c98afc5279",
-    To: "davidboxler47@gmail.com",
-    From: "davidboxler47@gmail.com",
+    SecureToken: "",
+    To: "",
+    From: "",
     Subject: subject.value,
     Body: bodyMessage,
-  }).then((message) => {
-        if(message == "OK") {
-            Swal.fire({
-                title: "Enviado!",
-                text: "Mensaje enviado correctamente!",
-                icon: "success"
-            })
+  }).then(() => {
+    Swal.fire({
+      title: "Enviado!",
+      text: "Mensaje enviado correctamente!",
+      icon: "success",
+    });
 
-            fullName.value = ""
-            email.value = ""
-            subject.value = ""
-            mess.value = ""
-        }        
-    }
-);
+    fullName.value = "";
+    email.value = "";
+    subject.value = "";
+    mess.value = "";
+  });
 }
 
 form.addEventListener("submit", (e) => {
